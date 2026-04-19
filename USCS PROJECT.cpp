0e20280dd6d45f70b5a7a81a68cd902ce6eb4f0e
amiqeem06 <amirulaqeem3@gmail.com>
2026-04-19 21:59:24 +0800

    Update USCS PROJECT.cpp
    update roomcost, fixed error sikit

--- USCS PROJECT.cpp
+++ USCS PROJECT.cpp
@@ -4,16 +4,17 @@
 #include<string>
 using namespace std;
 
-double flycost(int,int,int);
-double roomcost(int,int,int,int);
+double flycost(int,int, int,int);
+double roomcost(int,int);
 
 //global variable
 int flight;
-int accomodation;
+int accommodation;
 
 int main()
 {
 	int place ;//(1/2/3/4)
+	int kids, teenage, adults;
 	int days,numroom;//for accomodation calculation
 	int idcounter=1;
 	string action="yes";//to determine continue or not
@@ -37,9 +38,9 @@ int main()
 	cout<<"\n|    Langkawi    |     750      |    160    |    240    |   350    |";
 	cout<<"\n|________________|______________|___________|___________|__________|";
 	cout<<"\n";
-	cout<<"1.Kuching \t2.Terengganu \t3.Kota Kinabalu \t4.Langkawi";
+	cout<<"\n 1.Kuching \t2.Terengganu \t3.Kota Kinabalu \t4.Langkawi";
 
-	cout << " Choose the location of travel by entering this given numbers : " <<endl;
+	cout << " \n Choose the location of travel by entering this given numbers : " ;
 	cin >> place ;
 	
 	//number of people
@@ -56,8 +57,8 @@ int main()
 	cout<<"\nEnter number of days:";
 	//cin>>days;
 	
-	flight= double flycost(place,adults,kids,teenage);
-	accomodation = double roomcost(place,roomtype,days,numroom);
+	flight= flycost(place,adults,kids,teenage);
+	accommodation = roomcost(place, days);
 
 	
 	return 0;
@@ -65,73 +66,110 @@ int main()
 
 double flycost(int location,int dewasa,int kids1,int kids2)//calculate flight cost
 {
-	switch(location)
+	switch(location){
 	case 1:
-	flight=(adult*750)+(kids1*750*0.1)+(kids2*750*0.6);
+	flight=(dewasa*750)+(kids1*750*0.1)+(kids2*750*0.6);
 	break;
 	case 2:
-	flight=(adult*420)+(kids1*420*0.1)+(kids2*420*0.6);
+	flight=(dewasa*420)+(kids1*420*0.1)+(kids2*420*0.6);
 	break;
 	case 3:
-	flight=(adult*1300)+(kids1*1300*0.1)+(kids2*1300*0.6);
+	flight=(dewasa*1300)+(kids1*1300*0.1)+(kids2*1300*0.6);
 	break;
 	case 4:
-	flight=(adult*530)+(kids1*530*0.1)+(kids2*530*0.6);
+	flight=(dewasa*530)+(kids1*530*0.1)+(kids2*530*0.6);
 	break;
 	default: 
 	  cout << " Error. Please enter the given option.";
+}
 	return flight;
 }
 
-double roomcost(int location, int type, int night, int rooms)
+
+double roomcost ( int location, int staynight  ) //calculate room cost 
 {
-	double price;
-if(location==1)
-{
-if(type==1)
-	price=140;
-else if (type==2)
-	price=230;
-else
-	price=350;
-}
-else if(location==2)
-{
-if(type==1)
-	price=;
-else if(type==2)
-	price=;
-else
-	price=;
-}
-else if(location==3)
-{
-if(type==1)
-	price=;
-else if(type==2)
-	price=;
-else
-	price=;
-}
-else
-{
-if(type==1)
-	price=;
-else if(type==2)
-	price=;
-else
-	price=;
+	
+	char roomtype, more;
+	int single, deluxe, suite;
+	double roomprice, accommodation;
+	
+	switch (location)
+	{
+	
+	 case 1 :
+    
+		single = 140;
+		deluxe = 230;
+		suite = 350; 
+		break;
+	
+     case 2 :
+		single = 150;
+		deluxe = 220;
+		suite = 330;
+	    break ;
+	    
+     case 3 :
+		single = 120;
+		deluxe = 180;
+		suite = 275;
+        break ;
+        
+     case 4 :
+	
+		single = 160;
+		deluxe = 240;
+		suite = 350;
+	    break ;
+	    
+	default :
+		cout << " Error. Please use the given number. "<<endl;
+		exit(1) ;
+		
+	}
+
+
+do {
+    cout << " \n Single (S) | Deluxe (D) | Suite (U) "<<endl;
+	
+	cout << " Choose the room type  : " ;
+	cin >> roomtype; 
+
+	if ( roomtype == 'S'||roomtype =='s')
+	{
+		roomprice = single;
+	}
+	else if ( roomtype == 'D'||roomtype =='d')
+	{
+		roomprice = deluxe ;
+	}
+	else if ( roomtype == 'U'||roomtype =='u')
+	{
+		roomprice = suite ;
+	}
+	else 
+	{
+		cout << " Error. Please use the given letter. "<<endl;
+	}
+	
+	accommodation += (roomprice * staynight);
+	
+	cout << " \n Would you like to add another room? (Y/N): ";
+	cin >> more;
+	
+    }
+
+ 	while ( more == 'Y' || more == 'y');
+	
+	if ( staynight > 7)
+	{
+		accommodation = accommodation * 0.90;
+	}
+	
+	return accommodation ;
+	
 }
 
-double totalroom = price*night*rooms;
-
-if(night>7)
-{
-totalroom=totalroom*0.9;
-}
-
-return totalroom;
-}
 
 
 
